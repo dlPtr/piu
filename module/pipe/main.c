15a0eb6d740a3215da6c1726427816fdcad19a62
dlPtr <xxn.dlptr@gmail.com>
2020-04-19 01:03:07 +0800

    [ADD] add pipe demo

---
+++ module/pipe/main.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+
+#include "misc.h"
+
+int main(int argc, char *argv[])
+{
+    // return simplex_com(argc, argv);
+    return duplex_com(argc, argv);
+}
---
+++ module/pipe/misc.h
@@ -0,0 +1,9 @@
+#ifndef _MISC_H
+#define _MISC_H
+
+#define SLEN(x) x, sizeof(x)
+
+int simplex_com(int argc, char *argv[]);
+int duplex_com(int argc, char *argv[]);
+
+#endif /* _MISC_H */
